8577c30e62f03597b01c9cf6ef9fae82c0eac8ba
Faith <faithcalma07@gmail.com>
2025-07-17 13:18:02 +0800

    Add DueMinder component and update routing in main application

---
+++ frontend/src/pages/dueminder.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+function DueMinder() {
+    // const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //   // Simulate loading time (e.g., fetching data or assets)
+    //   const timer = setTimeout(() => {
+    //     setLoading(false);
+    //   }, 1000); // 3 seconds
+
+    //   return () => clearTimeout(timer);
+    // }, []);
+
+    // if (loading) {
+    //   return (
+    //     <div className="flex flex-col items-center justify-center h-screen bg-black text-white gap-3">
+    //       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-white"></div>
+    //       <h1 className="text-xl font-bold text-[#e7deda]">
+    //         Welcome to <span></span>DueMinder
+    //       </h1>
+    //     </div>
+    //   );
+    // }
+
+  return (
+    <></>
+  );
+}
+
+export default DueMinder;
